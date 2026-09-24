759454aacf5b49ad8be1cc21c7ef9b3ed4cfb9e0
imranonweb <alimranemon93@gmail.com>
2026-09-24 16:53:41 +0600

    rename page component to DocsPage and add ShowcasePage component

--- src/app/docs/page.tsx
+++ src/app/docs/page.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
 
-const page = () => {
+const DocsPage = () => {
     return (
         <div>
-            
+            <h2>Our Official Docs</h2>
         </div>
     );
 };
 
-export default page;
+export default DocsPage;
